6cc5e0efba8e5e317628aa7e7912f98f12f20090
Jacob Holloway <jhollowayj@users.noreply.github.com>
2016-08-27 12:31:32 -0600

    Now working copy
    Last commit didn't compile on a few things, but this one does.  Should be ready to integrate with programs.
    
    Still need to document a bit more and figure out workers if I end up having too many (since I limit the nPS to be <= len(hostnames).

--- slurm_manager.py
+++ slurm_manager.py
@@ -7,7 +7,7 @@ import re
 ## it may be useful to know that slurm_nodeid tells you which node you are one (in case there is more than one task on any given node...)
 ## Perhaps you could better assign parameter servers be distributed across all nodes before doubleing up on one.
 class SlurmClusterManager():
-    def __init__(self, num_param_servers=1, num_workers=None, default_starting_port=None):
+    def __init__(self, num_param_servers=1, num_workers=None, starting_port=None):
         
         # Check Environment for all needed SLURM varialbes
         assert 'SLURM_JOB_NODELIST' in os.environ # SLURM_NODELIST for backwards compatability if needed.
@@ -20,36 +20,39 @@ class SlurmClusterManager():
         self.hostnames = hostlist.expand_hostlist(os.environ['SLURM_JOB_NODELIST'])
         self.num_tasks_per_host = self._parse_slurm_tasks_per_node(os.environ['SLURM_TASKS_PER_NODE'])
         self.my_proc_id = int(os.environ['SLURM_PROCID'])
-        self.nprocs = int(os.environ['SLURM_NPROCS'])
+        self.num_processes = int(os.environ['SLURM_NPROCS'])
         self.nnodes = int(os.environ['SLURM_NNODES'])
 
         # Sanity check that everything has been parsed correctly
         assert len(self.hostnames) == len(self.num_tasks_per_host)
         assert len(self.hostnames) == self.nnodes
-        assert self.nprocs == sum(self.num_tasks_per_host)
+        assert self.num_processes == sum(self.num_tasks_per_host)
 
         # Numbber of PS/Workers
         # Note: I'm making the assumption that having more than one PS/node
         #       doesn't add any benefit.  It makes code simpler in self.build_cluster_spec()
-        self.nPS = min(num_param_servers, len(self.hostnames) 
+        self.num_parameter_servers = min(num_param_servers, len(self.hostnames))
         if num_workers is None:
-            # What happens to nWorkers once they 
-            self.nWorkers = self.nprocs - self.nPS # default to all other nodes doing something
+            # What happens to num_workers once I allocate less PS than they requested?
+            # Currently I'm not using num_workers'
+            self.num_workers = self.num_processes - self.num_parameter_servers # default to all other nodes doing something
 
         # Default port to use (this could probably be refactored... :\)
-        if default_port is not None:
-             self.default_port = default_port # use user specified port
+        if starting_port is not None:
+             self.starting_port = starting_port # use user specified port
         else:
             if 'SLURM_STEP_RESV_PORTS' in os.environ:
                 # Some slurm configureations have the ability to reserve ports, so use that if it's enabled.
-                self.default_port = int(os.environ['SLURM_STEP_RESV_PORTS'].split('-')[0])
+                self.starting_port = int(os.environ['SLURM_STEP_RESV_PORTS'].split('-')[0])
             else:
-                self.default_port = 2222
+                self.starting_port = 2222
         
 
     def build_cluster_spec(self):
-                # tuples of (str(Hostname:Port), JobName, TaskID) for each process
-        proc_info = [(None, None, None)] * self.nprocs 
+        # tuples of (str(Hostname:Port), JobName, TaskID) for each process
+        proc_info = []
+        for _ in range(self.num_processes):
+            proc_info.append([None, None, None]) 
 
         # Reverse-Lookup map
         first_pid_per_host = {}
@@ -59,39 +62,35 @@ class SlurmClusterManager():
         for cnt, hostname in zip(self.num_tasks_per_host, self.hostnames):
             first_pid_per_host[hostname] = pid
             for i in range(cnt):
-                proc_info[pid][0] "{}:{}".format(hostname, port + i)
+                proc_info[pid][0] = "{}:{}".format(hostname, self.starting_port + i)
                 pid += 1
 
         # Assign PSs to different physical hosts
+        # TODO Maybe sort hostnames(and task_per_hostname) by tasks_per_hostname??? 
         ps_strings = []
-        for ps_id in range(self.nPS):
+        for ps_id in range(self.num_parameter_servers):
             pid = first_pid_per_host[self.hostnames[ps_id]]
             ps_strings.append(proc_info[pid][0])
             proc_info[pid][1] = 'ps'
             proc_info[pid][2] = ps_id
-        
+
         # Assign workers to the remaining open spots 
         wk_id = 0
         wk_strings = []
         for info in proc_info:
-             if info[1] == None:
-                 wk_strings.append(info[pid][0])
-                 info[1] = 'worker'
-                 info[2] = wk_id
-                 wk_id += 1
-
-        # Build Cluster Spec
-        # spec = tf.train.ClusterSpec({'worker': wk_strings,
-        #                              'ps': ps_strings})
-        spec_dictionary = {'worker': wk_strings,
-                           'ps': ps_strings}
-
+            if info[1] == None:
+                wk_strings.append(info[0])
+                info[1] = 'worker'
+                info[2] = wk_id
+                wk_id += 1
+        
         # Grab your Job/TaskID
         job     = proc_info[self.my_proc_id][1]
         task_id = proc_info[self.my_proc_id][2]
 
         # Return it all!  :D
-        return spec_dictionary, job, task_id
+        cluster_spec = tf.train.ClusterSpec({'worker': wk_strings, 'ps': ps_strings})
+        return cluster_spec, job, task_id
 
     def _parse_slurm_tasks_per_node(self, num_tasks_per_nodes):
         '''
